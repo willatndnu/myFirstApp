8f3450b1d00907927b003d1da1792d3d07669da0
John Tumminaro <jtumminaro@thought-matrix.com>
2013-03-09 10:49:19 -0800

    Initial Commit

---
+++ myFirstApp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  myFirstApp
+//
+//  Created by John Tumminaro on 3/9/13.
+//  Copyright (c) 2013 John Tumminaro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
